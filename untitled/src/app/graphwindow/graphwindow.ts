67f19b4b9fde309cd14072a6f06fc3fccc487982
cedrik blankenhorn <cedrik.blankenhorn@gmail.com>
2026-01-23 12:18:53 +0100

    component erstellt

---
+++ untitled/src/app/graphwindow/graphwindow.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-graphwindow',
+  imports: [],
+  templateUrl: './graphwindow.html',
+  styleUrl: './graphwindow.css',
+})
+export class Graphwindow {
+
+}
